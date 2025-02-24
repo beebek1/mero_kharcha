c38dfdf5f02a34285162f97ea3c640b7bd1dba8d
SubodhCraft <nepaliboiz123@gmail.com>
2025-02-24 13:10:22 +0545

    Added a file

--- Sign-in.py
+++ Sign-in.py
@@ -77,11 +77,11 @@ code.bind('<FocusOut>',on_leave1)
 Frame(frame,width=295,height=2,bg='black').place(x=25,y=177)
 
 Button(frame,width=39,pady=7,text="Sign in",bg="#57a1f8",fg='white',border=0,command=signin).place(x=35,y=204)
+
 label=Label(frame,text="Don't have an account?",fg='black',bg='white',font=('Microsoft YaHei UI Light',9))
 label.place(x=75,y=270)
 
 sign_up=Button(frame,width=6,text="Sign up",border=0,bg="white",cursor='hand2',fg="#57a1f8")
 sign_up.place(x=215,y=270)
-
 
 root.mainloop()
